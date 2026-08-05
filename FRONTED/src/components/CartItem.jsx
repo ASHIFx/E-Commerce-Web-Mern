910f7e117cec6afde0adf0921256251ca94a2dbe
Asif <ashifgt12.12@gmail.com>
2026-08-05 15:45:56 +0530

    add start script for render deploy

--- FRONTED/src/components/CartItem.jsx
+++ FRONTED/src/components/CartItem.jsx
@@ -15,14 +15,15 @@ const CartItem = ({ item }) => {
     };
 
     return (
-        <div className='flex max-w-190 bg-zinc-900 border-zinc-700 border rounded-xl px-10 py-7 hover:border-orange-500 hover:translate-x-1 duration-300 transition-all'>
+        <div className="flex flex-col sm:flex-row w-full bg-zinc-900 border border-zinc-700 rounded-xl p-4 sm:p-6 hover:border-orange-500 hover:translate-x-1 transition-all duration-300">
             <img
                 src={item.imageUrl}
                 alt={item.name}
-                className='w-36 h-36 rounded-xl object-cover mr-10 border border-black/90'
+                className="w-28 h-28 sm:w-36 sm:h-36 rounded-xl object-cover mx-auto sm:mx-0 sm:mr-6"
             />
-            <div className='flex flex-col gap-2'>
-                <h3 className='text-xl font-semibold '>{item.name}</h3>
+            <div className="flex-1 flex flex-col gap-2 mt-4 sm:mt-0">
+                <h3 className="text-lg sm:text-xl font-semibold wrap-break-word">
+                    {item.name}</h3>
                 <p className='text-sm font-medium'>₹{item.price}</p>
                 <div className='flex mt-4 gap-3 items-center'>
                     <button
